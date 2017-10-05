764a3494c0c9ccc187115992a42bfc6d467f6aae
Marc Rousavy <marcrousavy@hotmail.com>
2017-10-05 15:46:45 +0200

    Basic error handling

--- src/com/mrousavy/services/AddSequence.java
+++ src/com/mrousavy/services/AddSequence.java
@@ -17,13 +17,17 @@ public class AddSequence {
 	  @GET // HTTP Get (= via URL) Metadata
 	  @Produces("application/xml") // Result Metadata (application/xml)
 	  public String addWithInput(@PathParam("s") String input) {
-		  String[] split = input.split(","); // Seperate by ","
-		  double total = 0;
-		  for (int i = 0; i < split.length; i++) { // Loop through each input
-			  total += Double.valueOf(split[i].trim()); // Add each string
+		  try {
+			  String[] split = input.split(","); // Seperate by ","
+			  double total = 0;
+			  for (int i = 0; i < split.length; i++) { // Loop through each input
+				  total += Double.valueOf(split[i].trim()); // Add each string
+			  }
+	
+			  String result = "addWithInput(string) Output: \n\nSequence Sum Output: \n\n" + total; // Build result with header
+		      return "<addseq>" + "<values>" + input + "</values><result>" + result + "</result>" + "</addseq>"; // Actual XML response
+		  } catch (Exception ex) {
+			  return "<addseq>Invalid input.</addseq>";
 		  }
-
-			String result = "addWithInput(string) Output: \n\nSequence Sum Output: \n\n" + total; // Build result with header
-			return "<addseq>" + "<values>" + input + "</values><result>" + result + "</result>" + "</addseq>"; // Actual XML response	  
 	  }
 }
--- src/com/mrousavy/services/SubstractSequence.java
+++ src/com/mrousavy/services/SubstractSequence.java
@@ -20,17 +20,21 @@ public class SubstractSequence {
 	  @GET // HTTP Get (= via URL) Metadata
 	  @Produces("application/json") // Result Metadata (application/json)
 	  public Response subWithInput(@PathParam("s") String input) {
-		  String[] split = input.split(","); // Seperate by ","
-		  double total = Double.valueOf(split[0]); // starting value is first value
-		  for (int i = 1; i < split.length; i++) { // Loop through each input
-			  total -= Double.valueOf(split[i].trim()); // Substract each value from "total", starting from index 1
-		  }
-
-		JSONObject jsonObject = new JSONObject(); // Build JSON Object from org.json
-		jsonObject.put("Input", input); // Push_back to JSON
-		jsonObject.put("Output", total); // Push_back to JSON
- 
-		String result = "subWithInput(string) Output: \n\nSubstract Sequence Service: \n\n" + jsonObject; // Actual JSON result
-		return Response.status(200).entity(result).build(); // Status 200: OK	  
+		  try {
+			  String[] split = input.split(","); // Seperate by ","
+			  double total = Double.valueOf(split[0]); // starting value is first value
+			  for (int i = 1; i < split.length; i++) { // Loop through each input
+				  total -= Double.valueOf(split[i].trim()); // Substract each value from "total", starting from index 1
+			  }
+			
+				JSONObject jsonObject = new JSONObject(); // Build JSON Object from org.json
+				jsonObject.put("Input", input); // Push_back to JSON
+				jsonObject.put("Output", total); // Push_back to JSON
+				 
+				String result = "subWithInput(string) Output: \n\nSubstract Sequence Service: \n\n" + jsonObject; // Actual JSON result
+				return Response.status(200).entity(result).build(); // Status 200: OK
+		  } catch (Exception ex) {
+			  return Response.status(200).entity("Invalid input.").build();
+		  }	  
 	  }
 }
