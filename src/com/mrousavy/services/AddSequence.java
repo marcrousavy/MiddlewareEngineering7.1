812f348cb0d31cfeacb5854ae94b71f81acc7b0e
Marc Rousavy <marcrousavy@hotmail.com>
2017-10-05 15:14:42 +0200

    Add new Service: Add Sequence (addseq)

---
+++ src/com/mrousavy/services/AddSequence.java
@@ -0,0 +1,23 @@
+package com.mrousavy.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+ 
+@Path("/addseq")
+public class AddSequence {
+	  @Path("{s}")
+	  @GET
+	  @Produces("application/xml")
+	  public String addWithInput(@PathParam("s") String input) {
+		  String[] split = input.split(","); // Seperate by ","
+		  double total = 0;
+		  for (int i = 0; i < split.length; i++) { // Loop through each input
+			  total += Double.valueOf(split[i].trim()); // Add each string
+		  }
+
+			String result = "@Produces(\"application/xml\") Output: \n\nSequence Sum Output: \n\n" + total; // Build result with header
+			return "<addseq>" + "<values>" + input + "</values><result>" + result + "</result>" + "</addseq>"; // Actual XML response	  
+	  }
+}
